30aa9a144f3fa68236dc9e73773c26ffac7ae79a
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-09-21 13:50:11 +0200

    ZadMove

---
+++ Rvalue/Rvalue.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+class A
+{
+private:
+    std::string _str = {};
+public:
+    A(std::string str) : _str(str)
+    {
+    }
+    A(A&& other) noexcept
+    {
+        this->_str = std::move(other._str);
+    }
+    A operator=(A&& other) noexcept
+    {
+        this->_str = std::move(other._str);
+    }
+
+};
+
+void swapOld(int& a, int& b)
+{
+    int temp(a); // tutaj wolamy konstruktor kopiujacy, mamy teraz 2 kopie a 
+    a = b;
+    b = temp;
+}
+
+void swapMove(int& a, int& b)
+{
+    int temp(std::move(a)); //jawnie zadamy zamiany obiektu na rvalue ref, nie robi sie kopia, 
+    //od teraz nie wolno odwoływac się do wartości a
+    a = std::move(b);
+    b = std::move(temp);
+}
+
+int main()
+{
+    A a("Hi");
+    A& a_ref = a;
+    A&& ref = A("Hello");
+}
---
+++ ZadMove/ZadMove.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+class K
+{
+private:
+    int _a = 0;
+public:
+    K()
+    {
+        std::cout << "Konstruktor bezparametrowy" << std::endl;
+    }
+
+    K (int a) : _a(a)
+    {
+        std::cout << "Konstruktor parametrowy" << std::endl;
+
+    }
+
+    K(const K& other) //konstruktor kopiujacy
+    {
+        std::cout << "Konstruktor kopiujacy" << std::endl;
+        this->_a = other._a;
+    }
+
+    K(K&& other) noexcept //konstruktor przenoszacy
+    {
+        std::cout << "Konstruktor przenoszacy" << std::endl;
+        this->_a = std::move(other._a);
+    }
+
+    int getA() const
+    {
+        return _a;
+    }
+};
+
+int main()
+{
+    K x(5);
+    K y(std::move(x));
+
+    std::cout
+        << "a od x: " << x.getA() << std::endl //tu nic nie ma, zostalo przeniesione do b, nie powinno się nic na tym obiekcie robic
+        << "a od y: " << y.getA() << std::endl;
+
+}
